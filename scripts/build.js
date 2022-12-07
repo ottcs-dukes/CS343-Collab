40b78bebf13031a7047543c153d158068c3f5265
OttCS <ottcs20@gmail.com>
2022-12-07 14:01:38 -0500

    Factory

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,22 @@
 let newsArticles;
 
+function buildCard(article) {
+  let outer = document.createElement('div');
+  outer.classList.add("card", "mt-5", "mx-auto", "mb-5");
+
+  let title = document.createElement('h1');
+  title.classList.add("card-header");
+  title.textContent = article.title;
+  outer.append(title);
+
+  let content = document.createElement('p');
+  content.classList.add("card-body");
+  content.textContent = article.content;
+  outer.append(content);
+
+  return outer
+}
+
 fetch('articles.json').then(response => {
   response.text().then((text) => {
     newsArticles = JSON.parse(text)["content"];
@@ -9,26 +26,8 @@ fetch('articles.json').then(response => {
 
     // iterate over the news articles and create HTML elements for each article
     for (const article of newsArticles) {
-      const articleElement = document.createElement('div');
-      const articleTitle = document.createElement('h1');
-      const articleAuthor = document.createElement('p');
-      const articleDate = document.createElement('p');
-      const articleContent = document.createElement('p');
-
-      // set the text content of the article's title, author, date, and content
-      articleTitle.textContent = article.title;
-      articleAuthor.textContent = article.author;
-      articleDate.textContent = article.date;
-      articleContent.textContent = article.content;
-
-      // add the article's title, author, date, and content to the article element
-      articleElement.appendChild(articleTitle);
-      articleElement.appendChild(articleAuthor);
-      articleElement.appendChild(articleDate);
-      articleElement.appendChild(articleContent);
-
       // add the article element to the target page
-      targetPage.appendChild(articleElement);
+      targetPage.appendChild(buildCard(article));
     }
   })
 });
